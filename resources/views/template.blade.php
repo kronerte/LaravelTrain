913852dd81953b9248e7dbac2b5dbd69460da1ca
kronerte <etienne.kronert@gmail.com>
2018-05-11 16:15:14 +0200

    facebook

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -65,6 +65,28 @@
         </style>
     </head>
     <body>
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 229407827833341,
+      cookie     : true,
+      xfbml      : true,
+      version    : 1
+    });
+
+    FB.AppEvents.logPageView();
+
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
         <div class="flex-center position-ref full-height">
 
                 <div class="top-right links">
